b86bb1ee3234ab097e8f5dc726182f456cecfdf2
Jean Carlos Marcelino <5326539+jeancm@users.noreply.github.com>
2022-08-26 20:57:07 -0300

    Add players online

--- tibia/js/tibia.js
+++ tibia/js/tibia.js
@@ -6,3 +6,16 @@ function submit_form() {
     upper_range = Math.round(level*1.5);
     content.innerHTML = content.innerHTML + "A character with level <b>" + level +"</b> can share experience with levels <b>" + lower_range + "</b> to <b>" + upper_range +"</b><br>";
 }
+
+async function playersOnline() {
+    content = document.getElementById("pagoderaPlayersOnline");
+    content.innerHTML = "?";
+    url = "https://api.pagodera.com/api/v1/online"
+    let response = await fetch(url);
+    if (response.status === 200) {
+        let data = await response.text();
+        content.innerHTML = JSON.parse(data).online;
+    }
+}
+
+playersOnline();
